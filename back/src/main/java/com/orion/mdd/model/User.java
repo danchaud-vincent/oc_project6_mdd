8061885e1c28fff232c787bf3ac10ba202813118
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-16 16:48:10 +0200

    add topics into user entity

--- back/src/main/java/com/orion/mdd/model/User.java
+++ back/src/main/java/com/orion/mdd/model/User.java
@@ -1,9 +1,17 @@
 package com.orion.mdd.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +26,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Integer userId;
 
     private String username;
@@ -25,5 +34,11 @@ public class User {
     private String email;
 
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST
+    })
+    @JoinTable(name = "user_topic", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "topic_id"))
+    private List<Topic> topics;
 
 }
