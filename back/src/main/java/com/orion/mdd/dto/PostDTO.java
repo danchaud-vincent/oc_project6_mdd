4208e86ad802b48560c2be29d6b7a10226b37a51
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-17 19:22:50 +0200

    create DTOs for user, topic and Post

---
+++ back/src/main/java/com/orion/mdd/dto/PostDTO.java
@@ -0,0 +1,21 @@
+package com.orion.mdd.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDTO {
+
+    private Integer postId;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private TopicDTO topic;
+    private UserDTO author;
+
+}
---
+++ back/src/main/java/com/orion/mdd/dto/TopicDTO.java
@@ -0,0 +1,15 @@
+package com.orion.mdd.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TopicDTO {
+
+    private String title;
+    private String description;
+
+}
