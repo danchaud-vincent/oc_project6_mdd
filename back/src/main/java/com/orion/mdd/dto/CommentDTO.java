808ec40761f8af905696e65a450359a99bb41776
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-17 19:30:23 +0200

    create a comment dto

---
+++ back/src/main/java/com/orion/mdd/dto/CommentDTO.java
@@ -0,0 +1,19 @@
+package com.orion.mdd.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDTO {
+
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private UserDTO createdBy;
+
+}
