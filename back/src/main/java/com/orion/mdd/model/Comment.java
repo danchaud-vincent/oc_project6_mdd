75f705b59516d0bf4e887d3024b2337ed0e818ff
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-17 18:24:47 +0200

    correct comment entity

--- back/src/main/java/com/orion/mdd/model/Comment.java
+++ back/src/main/java/com/orion/mdd/model/Comment.java
@@ -1,10 +1,18 @@
 package com.orion.mdd.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,5 +33,21 @@ public class Comment {
     private Integer commentId;
 
     private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", updatable = true)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
 
 }
