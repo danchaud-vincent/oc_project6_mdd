6f70730fc8c026fb28352624cec9691ae81425ce
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-18 09:37:18 +0200

    add a createPost method in post controller

--- back/src/main/java/com/orion/mdd/controller/PostController.java
+++ back/src/main/java/com/orion/mdd/controller/PostController.java
@@ -10,25 +10,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orion.mdd.dto.PostDTO;
+import com.orion.mdd.payload.request.PostCreateRequest;
 import com.orion.mdd.service.PostService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/posts")
 public class PostController {
 
     private final PostService postService;
 
-    @GetMapping("/posts")
+    @GetMapping()
     public ResponseEntity<List<PostDTO>> getPosts() {
         return new ResponseEntity<List<PostDTO>>(postService.getPosts(), HttpStatus.OK);
     }
 
-    @GetMapping("/posts/{postId}")
+    @GetMapping("/{postId}")
     public ResponseEntity<PostDTO> getPostById(@PathVariable Integer postId) {
         return new ResponseEntity<PostDTO>(postService.getPostById(postId), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public String createPost(@RequestBody PostCreateRequest postCreateRequest) {
+
+        return "Post Created";
     }
 
 }
