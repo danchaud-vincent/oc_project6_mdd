1029e3583d2b1052c3fa0f9a97cf2c0b6d31278b
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-18 11:18:04 +0200

    correst post mapper by adding @Mapping annotations

--- back/src/main/java/com/orion/mdd/mapper/PostMapper.java
+++ back/src/main/java/com/orion/mdd/mapper/PostMapper.java
@@ -2,10 +2,29 @@ package com.orion.mdd.mapper;
 
 import com.orion.mdd.dto.PostDTO;
 import com.orion.mdd.model.Post;
+import com.orion.mdd.model.Topic;
+import com.orion.mdd.model.User;
+import com.orion.mdd.payload.request.PostCreateRequest;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper extends EntityMapper<PostDTO, Post> {
+
+    @Override
+    @Mapping(source = "author.username", target = "authorName")
+    @Mapping(source = "topic.name", target = "topicName")
+    PostDTO toDto(Post post);
+
+    @Override
+    @Mapping(source = "authorName", target = "author.username")
+    @Mapping(source = "topicName", target = "topic.name")
+    Post toEntity(PostDTO postDTO);
+
+    @Mapping(target = "postId", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Post toEntity(PostCreateRequest postRequest, User author, Topic topic);
 
 }
