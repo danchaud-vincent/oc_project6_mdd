d47911a7e303806d7f29e684ce2378ab3ebfd093
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-18 15:34:12 +0200

    add method to find comments by id in comment repo

--- back/src/main/java/com/orion/mdd/repository/CommentRepository.java
+++ back/src/main/java/com/orion/mdd/repository/CommentRepository.java
@@ -1,5 +1,7 @@
 package com.orion.mdd.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.orion.mdd.model.Comment;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    List<Comment> findByPostId(Integer postId);
 
 }
