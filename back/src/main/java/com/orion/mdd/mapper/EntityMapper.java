cb0fca73a84b62a8c02ee5d1be0274d6bc403dc8
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-18 15:50:28 +0200

    remove useless code in entity mapper

--- back/src/main/java/com/orion/mdd/mapper/EntityMapper.java
+++ back/src/main/java/com/orion/mdd/mapper/EntityMapper.java
@@ -4,12 +4,6 @@ import java.util.List;
 
 public interface EntityMapper<D, E> {
 
-    E toEntity(D dto);
-
-    D toDto(E entity);
-
-    List<E> toEntity(List<D> dtoList);
-
     List<D> toDto(List<E> entityList);
 
 }
