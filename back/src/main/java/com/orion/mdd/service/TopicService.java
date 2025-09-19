a467256820d48f07dd8991a11f86b971418c9184
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-19 10:15:49 +0200

    create topic service and add method getTopics

---
+++ back/src/main/java/com/orion/mdd/service/TopicService.java
@@ -0,0 +1,28 @@
+package com.orion.mdd.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.orion.mdd.dto.TopicDTO;
+import com.orion.mdd.mapper.TopicMapper;
+import com.orion.mdd.model.Topic;
+import com.orion.mdd.repository.TopicRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TopicService {
+
+    private final TopicRepository topicRepository;
+    private final TopicMapper topicMapper;
+
+    public List<TopicDTO> getTopics() {
+
+        List<Topic> topics = topicRepository.findAll();
+
+        return topicMapper.toDto(topics);
+    }
+
+}
