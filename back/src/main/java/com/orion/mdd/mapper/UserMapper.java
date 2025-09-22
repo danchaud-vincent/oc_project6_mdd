0a1c9f8b4b105a8a979a3db4e800ce87907eb228
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-22 16:04:16 +0200

    add mappers in userMapper

--- back/src/main/java/com/orion/mdd/mapper/UserMapper.java
+++ back/src/main/java/com/orion/mdd/mapper/UserMapper.java
@@ -1,8 +1,25 @@
 package com.orion.mdd.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.orion.mdd.dto.UserDTO;
+import com.orion.mdd.model.User;
+import com.orion.mdd.payload.request.RegisterRequest;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "topics", ignore = true)
+    User toEntity(RegisterRequest registerRequest);
+
+    UserDTO tDto(User user);
+
+    List<UserDTO> toDto(List<User> userList);
+
+    List<User> toEntity(List<UserDTO> userDtoList);
 
 }
