14f589b146d0d69cbde80baf53aebc1833f5c8a7
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-22 11:41:44 +0200

    add findByEmail method in user repo

--- back/src/main/java/com/orion/mdd/repository/UserRepository.java
+++ back/src/main/java/com/orion/mdd/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.orion.mdd.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.orion.mdd.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String userEmail);
 
 }
