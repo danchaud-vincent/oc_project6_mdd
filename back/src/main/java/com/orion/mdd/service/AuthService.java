cf0e53072c6700c343777fd3fe7f221e5fe2306b
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-22 16:05:03 +0200

    modify register method in AuthService by adding password encoder and user mapper

--- back/src/main/java/com/orion/mdd/service/AuthService.java
+++ back/src/main/java/com/orion/mdd/service/AuthService.java
@@ -1,7 +1,13 @@
 package com.orion.mdd.service;
 
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.orion.mdd.dto.UserDTO;
+import com.orion.mdd.mapper.UserMapper;
+import com.orion.mdd.model.User;
 import com.orion.mdd.payload.request.LoginRequest;
 import com.orion.mdd.payload.request.RegisterRequest;
 import com.orion.mdd.repository.UserRepository;
@@ -13,15 +19,30 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public String authenticate(LoginRequest loginRequest) {
 
         return "authenticated";
     }
 
-    public String register(RegisterRequest registerRequest) {
+    public UserDTO register(RegisterRequest registerRequest) {
 
-        return "registered";
+        // check if user don't already exist
+        String email = registerRequest.getEmail();
+        Optional<User> existingUser = userRepository.findByEmail(email);
+
+        if (existingUser.isPresent()) {
+            throw new RuntimeException(String.format("User with email %s already exists!", email));
+        }
+
+        // add the new user
+        User newUser = userMapper.toEntity(registerRequest);
+        newUser.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        User newUserSaved = userRepository.save(newUser);
+
+        return userMapper.tDto(newUserSaved);
     }
 
 }
