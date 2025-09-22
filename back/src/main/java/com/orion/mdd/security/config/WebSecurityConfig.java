9194e49cf6333ffbff0dae7b423584df781740e6
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-22 18:35:18 +0200

    update the SecurityConfig class with AuthManager, password encoder, oauth2ResourceServer

--- back/src/main/java/com/orion/mdd/security/config/WebSecurityConfig.java
+++ back/src/main/java/com/orion/mdd/security/config/WebSecurityConfig.java
@@ -29,8 +29,8 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
 
     }
