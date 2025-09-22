bbe8397b626c1ee837bdb5a252c98a217160ed95
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-22 18:35:55 +0200

    update login and register method in auth controller

--- back/src/main/java/com/orion/mdd/controller/AuthController.java
+++ back/src/main/java/com/orion/mdd/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.orion.mdd.dto.UserDTO;
 import com.orion.mdd.payload.request.LoginRequest;
 import com.orion.mdd.payload.request.RegisterRequest;
+import com.orion.mdd.payload.response.JwtResponse;
 import com.orion.mdd.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,14 +24,12 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-
-        return new ResponseEntity<String>(authService.authenticate(loginRequest), HttpStatus.OK);
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest loginRequest) {
+        return new ResponseEntity<JwtResponse>(authService.authenticate(loginRequest), HttpStatus.OK);
     }
 
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@RequestBody RegisterRequest registerRequest) {
-
         return new ResponseEntity<UserDTO>(authService.register(registerRequest), HttpStatus.OK);
     }
 
