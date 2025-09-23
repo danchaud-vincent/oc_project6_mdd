dbfcbdbffe26dd415b41bfad7f5afce893a211d7
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-23 11:00:14 +0200

    add ResourceNotFoundException handler

--- back/src/main/java/com/orion/mdd/exception/handler/GlobalExceptionHandler.java
+++ back/src/main/java/com/orion/mdd/exception/handler/GlobalExceptionHandler.java
@@ -1,8 +1,22 @@
 package com.orion.mdd.exception.handler;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.orion.mdd.exception.custom.ResourceNotFoundException;
+import com.orion.mdd.payload.response.ErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
+    }
 
 }
