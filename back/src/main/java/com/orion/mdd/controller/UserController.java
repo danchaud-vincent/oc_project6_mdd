ae0b9ad82ae4804d50d662769485b3b6c0cfed60
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-23 11:51:26 +0200

    create getUserById and UpdateUserById in usercontroller

--- back/src/main/java/com/orion/mdd/controller/UserController.java
+++ back/src/main/java/com/orion/mdd/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.orion.mdd.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.orion.mdd.dto.UserDTO;
 import com.orion.mdd.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +20,20 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Integer userId) {
+
+        UserDTO userDTO = userService.getUserById(userId);
+
+        return new ResponseEntity<UserDTO>(userDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/{userId}")
+    public String updateUserById(@PathVariable String userId, @RequestBody String entity) {
+        // TODO: process PUT request
+
+        return entity;
+    }
 
 }
