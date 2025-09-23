83350ec2f39111754ce77b8d5ca31b316142d096
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-23 19:11:09 +0200

    add subscribe method i topic service and user authenticaiton to find the user principal

--- back/src/main/java/com/orion/mdd/service/TopicService.java
+++ back/src/main/java/com/orion/mdd/service/TopicService.java
@@ -2,13 +2,18 @@ package com.orion.mdd.service;
 
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import com.orion.mdd.dto.TopicDTO;
+import com.orion.mdd.exception.custom.ResourceNotFoundException;
 import com.orion.mdd.mapper.TopicMapper;
 import com.orion.mdd.model.Topic;
+import com.orion.mdd.model.User;
 import com.orion.mdd.payload.request.TopicRequest;
 import com.orion.mdd.repository.TopicRepository;
+import com.orion.mdd.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class TopicService {
 
     private final TopicRepository topicRepository;
+    private final UserRepository userRepository;
     private final TopicMapper topicMapper;
 
     public List<TopicDTO> getTopics() {
@@ -32,6 +38,26 @@ public class TopicService {
         Topic topicSaved = topicRepository.save(newTopic);
 
         return topicMapper.toDto(topicSaved);
+    }
+
+    public List<TopicDTO> subscribeToTopic(Integer topicId, Authentication authentication) {
+        // Get user principal
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        String emailPrincipal = jwt.getClaim("sub");
+
+        User userPrincipal = userRepository.findByEmail(emailPrincipal)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        String.format("User with email %s Not Found", emailPrincipal)));
+
+        // Find topic
+        Topic topic = topicRepository.findById(topicId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Topic with ID %s Not Found", topicId)));
+
+        // add topic to user
+        userPrincipal.getTopics().add(topic);
+        User userSaved = userRepository.save(userPrincipal);
+
+        return topicMapper.toDto(userSaved.getTopics());
     }
 
 }
