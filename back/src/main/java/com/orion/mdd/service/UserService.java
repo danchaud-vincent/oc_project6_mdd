aabb579e25cefcd0442d0a5d7a42695debb066ad
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-23 11:51:40 +0200

    create getUserById in user service

--- back/src/main/java/com/orion/mdd/service/UserService.java
+++ back/src/main/java/com/orion/mdd/service/UserService.java
@@ -2,6 +2,10 @@ package com.orion.mdd.service;
 
 import org.springframework.stereotype.Service;
 
+import com.orion.mdd.dto.UserDTO;
+import com.orion.mdd.exception.custom.ResourceNotFoundException;
+import com.orion.mdd.mapper.UserMapper;
+import com.orion.mdd.model.User;
 import com.orion.mdd.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +15,15 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserDTO getUserById(Integer userId) {
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with ID %s Not Found", userId)));
+
+        UserDTO userDTO = userMapper.tDto(existingUser);
+
+        return userDTO;
+    }
 
 }
