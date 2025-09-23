5bb1f50beb12cdcc532bd67965b40ad9090aaabc
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-23 19:10:41 +0200

    add subscribe method in Topic controller with the use of Authentication to find the user principal

--- back/src/main/java/com/orion/mdd/controller/TopicController.java
+++ back/src/main/java/com/orion/mdd/controller/TopicController.java
@@ -13,7 +13,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -38,6 +40,14 @@ public class TopicController {
         TopicDTO topicDTO = topicService.createTopic(topicRequest);
 
         return new ResponseEntity<TopicDTO>(topicDTO, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{topicId}/subscribe")
+    public ResponseEntity<List<TopicDTO>> subscribe(@PathVariable Integer topicId, Authentication authentication) {
+
+        List<TopicDTO> topicsDTO = topicService.subscribeToTopic(topicId, authentication);
+
+        return new ResponseEntity<List<TopicDTO>>(topicsDTO, HttpStatus.OK);
     }
 
 }
