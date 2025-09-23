0f13e77e8b41384374639fd648839197bdc1ca6e
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-23 11:27:21 +0200

    add UsernameNotFound and BadCredentials exceptions in globalExceptionhandler

--- back/src/main/java/com/orion/mdd/exception/handler/GlobalExceptionHandler.java
+++ back/src/main/java/com/orion/mdd/exception/handler/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package com.orion.mdd.exception.handler;
 import java.util.Date;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.orion.mdd.exception.custom.ResourceNotFoundException;
+import com.orion.mdd.exception.custom.UserAlreadyExistsException;
 import com.orion.mdd.payload.response.ErrorResponse;
 
 @RestControllerAdvice
@@ -17,6 +20,24 @@ public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException ex) {
         return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
+    }
+
+    @ExceptionHandler(value = UserAlreadyExistsException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    public ErrorResponse handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage(), new Date());
+    }
+
+    @ExceptionHandler(value = UsernameNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorResponse handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
+    }
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleBadCredentialsException(BadCredentialsException ex) {
+        return new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), new Date());
     }
 
 }
