63ae67c220574c95c463c35de14302018468123d
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-23 11:55:28 +0200

    add updateFromrequest in usermapper when updating a user

--- back/src/main/java/com/orion/mdd/mapper/UserMapper.java
+++ back/src/main/java/com/orion/mdd/mapper/UserMapper.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import com.orion.mdd.dto.UserDTO;
 import com.orion.mdd.model.User;
 import com.orion.mdd.payload.request.RegisterRequest;
+import com.orion.mdd.payload.request.UserRequest;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -19,4 +21,8 @@ public interface UserMapper {
     UserDTO tDto(User user);
 
     List<UserDTO> toDto(List<User> userList);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "topics", ignore = true)
+    void updateFromRequest(UserRequest userRequest, @MappingTarget User existingUser);
 }
