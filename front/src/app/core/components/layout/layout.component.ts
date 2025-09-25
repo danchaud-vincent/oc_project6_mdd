64243a1e84ad4d9d8d37ed31b387c63c0f10938a
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-25 19:17:17 +0200

    add styling, change the html to be exactly as the design wants, add fixedTopGrad observable in ts

--- front/src/app/core/components/layout/layout.component.ts
+++ front/src/app/core/components/layout/layout.component.ts
@@ -5,15 +5,23 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, MatSidenavModule, MatListModule, MatIconModule],
+  imports: [
+    HeaderComponent,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
 export class LayoutComponent implements OnInit {
   isMobile$!: Observable<boolean>;
+  fixedTopGap$!: Observable<number>;
 
   constructor(private breakPointObserver: BreakpointObserver) {}
 
@@ -21,5 +29,9 @@ export class LayoutComponent implements OnInit {
     this.isMobile$ = this.breakPointObserver
       .observe([Breakpoints.XSmall, Breakpoints.Small])
       .pipe(map((result) => result.matches));
+
+    this.fixedTopGap$ = this.isMobile$.pipe(
+      map((isMobile) => (isMobile ? 56 : 64))
+    );
   }
 }
