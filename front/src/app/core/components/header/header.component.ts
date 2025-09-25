e81affe53cb7be1a822339df695d3f5154c1f3d8
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-25 12:31:17 +0200

    import mat sidebar, icon, list, button to create a nav sidebar in header component

--- front/src/app/core/components/header/header.component.ts
+++ front/src/app/core/components/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, MatToolbarModule],
+  imports: [
+    RouterLink,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
