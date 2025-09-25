b925a72634b8f3783e957ec97c380d41dca42863
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-25 17:40:56 +0200

    add BreakPointObserver in layout component

--- front/src/app/core/components/layout/layout.component.ts
+++ front/src/app/core/components/layout/layout.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -10,4 +12,14 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit {
+  isMobile$!: Observable<boolean>;
+
+  constructor(private breakPointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.isMobile$ = this.breakPointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(map((result) => result.matches));
+  }
+}
