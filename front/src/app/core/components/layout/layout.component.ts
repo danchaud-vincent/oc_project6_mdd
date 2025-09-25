bc1677ebce9576c4db321c0f84671b1c081c0854
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-25 16:55:59 +0200

    remove unused imports, and import MatSidenavModule

--- front/src/app/core/components/layout/layout.component.ts
+++ front/src/app/core/components/layout/layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MatSidenavModule, MatListModule, MatIconModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
