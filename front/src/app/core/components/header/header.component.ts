9dbac6ed050a42824ba1981009abc0a0200f66f2
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-25 17:42:19 +0200

    clean the code, add inputs to deal with Mobile View and MatSidenav toggle

--- front/src/app/core/components/header/header.component.ts
+++ front/src/app/core/components/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +14,14 @@ import { MatListModule } from '@angular/material/list';
     RouterLink,
     MatToolbarModule,
     MatIconModule,
-    MatSidenavModule,
     MatButtonModule,
     MatListModule,
+    AsyncPipe,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() matSidenav!: MatSidenav;
+  @Input() isMobile$!: Observable<boolean>;
+}
