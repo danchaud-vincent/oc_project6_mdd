0056ae15e69260d098196989d9f122dea4bd9be4
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-26 10:25:32 +0200

    rename layout to layoutWithHeaders component

---
+++ front/src/app/core/components/layout-with-headers/layout-with-headers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    HeaderComponent,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+  ],
+  templateUrl: './layout-with-headers.component.html',
+  styleUrl: './layout-with-headers.component.scss',
+})
+export class LayoutWithHeadersComponent implements OnInit {
+  isMobile$!: Observable<boolean>;
+  fixedTopGap$!: Observable<number>;
+
+  constructor(private breakPointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.isMobile$ = this.breakPointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(map((result) => result.matches));
+
+    this.fixedTopGap$ = this.isMobile$.pipe(
+      map((isMobile) => (isMobile ? 56 : 64))
+    );
+  }
+}
