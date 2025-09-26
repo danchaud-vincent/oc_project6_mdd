64069168769e9c950ac0de5e023c07f472f22848
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-26 10:25:49 +0200

    modify the routes due to renaming

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './core/components/layout/layout.component';
+import { LayoutWithHeadersComponent } from './core/components/layout-with-headers/layout-with-headers.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
+    component: LayoutWithHeadersComponent,
   },
 ];
