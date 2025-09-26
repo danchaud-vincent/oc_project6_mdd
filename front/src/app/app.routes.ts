f005e354bd81cb51bb260a14e753a98c3eeea29d
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-26 11:54:43 +0200

    change app routes to add home component as a children in layoutComponent

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { LayoutWithHeadersComponent } from './core/components/layouts/layout-with-headers/layout-with-headers.component';
+import { LayoutComponent } from './core/components/layouts/layout/layout.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutWithHeadersComponent,
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ],
   },
 ];
