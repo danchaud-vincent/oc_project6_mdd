a3378df9c24e2938022be816ac4cc11d5d25b52e
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-29 16:08:25 +0200

    modify JwtResponse by adding user info

--- back/src/main/java/com/orion/mdd/payload/response/JwtResponse.java
+++ back/src/main/java/com/orion/mdd/payload/response/JwtResponse.java
@@ -1,14 +1,19 @@
 package com.orion.mdd.payload.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class JwtResponse {
 
+    private Integer id;
+    private String username;
+    private String email;
     private String token;
 
 }
