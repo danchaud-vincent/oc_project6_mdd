4384d6c9256da8f49ed40d8d18efc692ec739132
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-29 18:29:28 +0200

    add angular material elements in layout-auth html

--- front/src/app/core/components/layouts/layout-auth/layout-auth.component.ts
+++ front/src/app/core/components/layouts/layout-auth/layout-auth.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout-auth',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, RouterOutlet],
   templateUrl: './layout-auth.component.html',
-  styleUrl: './layout-auth.component.scss'
+  styleUrl: './layout-auth.component.scss',
 })
-export class LayoutAuthComponent {
-
-}
+export class LayoutAuthComponent {}
