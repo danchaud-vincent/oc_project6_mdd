1c007ec4a3ac0dea64a32b5ae47eb36e4aabd0df
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-29 16:59:58 +0200

    add CrossOrigin annotation in controllers

--- back/src/main/java/com/orion/mdd/controller/AuthController.java
+++ back/src/main/java/com/orion/mdd/controller/AuthController.java
@@ -13,9 +13,11 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
--- back/src/main/java/com/orion/mdd/controller/TopicController.java
+++ back/src/main/java/com/orion/mdd/controller/TopicController.java
@@ -16,11 +16,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/topics")
