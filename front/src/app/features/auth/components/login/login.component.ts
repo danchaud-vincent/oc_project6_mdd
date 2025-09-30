6d5eec7b831bd21dd665f33e534f07d26cde28d5
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-30 12:19:01 +0200

    create a login form with css styling, formGroup and formBuilder

--- front/src/app/features/auth/components/login/login.component.ts
+++ front/src/app/features/auth/components/login/login.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  emailRegex!: RegExp;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.pattern(this.emailRegex)]],
+      password: [null, Validators.required],
+    });
+  }
+
+  onLogin(): void {
+    console.log(this.loginForm.value);
+  }
 }
