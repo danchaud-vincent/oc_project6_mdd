40b5b4b444ab41a3adab2fa12f47691caa07f3bc
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-30 18:15:28 +0200

    add register method in auth service

--- front/src/app/features/auth/services/auth.service.ts
+++ front/src/app/features/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LoginRequest } from '../models/loginRequest.model';
+import { RegisterRequest } from '../models/registerRequest.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +10,17 @@ import { LoginRequest } from '../models/loginRequest.model';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  login(loginRequest: LoginRequest): Observable<String> {
-    return this.http.post<String>(
+  login(loginRequest: LoginRequest): Observable<string> {
+    return this.http.post<string>(
       'http://localhost:3001/api/auth/login',
       loginRequest
+    );
+  }
+
+  register(registerRequest: RegisterRequest): Observable<string> {
+    return this.http.post<string>(
+      'http://localhost:3001/api/auth/register',
+      registerRequest
     );
   }
 }
