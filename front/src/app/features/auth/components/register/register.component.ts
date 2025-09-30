596a4ff6d77484348ccbe15956e21e81e76c607f
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-30 17:15:48 +0200

    create form for the register component, add styling

--- front/src/app/features/auth/components/register/register.component.ts
+++ front/src/app/features/auth/components/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
