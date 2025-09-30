f2082a891ecfa5cdf527ebf7fb06ce1fb814c461
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-30 17:48:39 +0200

    add getFormControlErrorText method and mat-error in register form to deal with validators

--- front/src/app/features/auth/components/register/register.component.ts
+++ front/src/app/features/auth/components/register/register.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,4 +17,46 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  passwordRegexp!: RegExp;
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.passwordRegexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+    this.registerForm = this.formbuilder.group({
+      username: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(this.passwordRegexp),
+        ],
+      ],
+    });
+  }
+
+  onRegister() {
+    console.log(this.registerForm.value);
+  }
+
+  getFormControlErrorText(ctrl: AbstractControl): string {
+    if (ctrl.hasError('required')) {
+      return 'Ce champs est requis';
+    } else if (ctrl.hasError('email')) {
+      return "Merci d'entrer une adresse mail valide";
+    } else if (ctrl.hasError('minlength')) {
+      return 'Ce mot de passe ne contient pas assez de caractères';
+    } else {
+      return 'Ce champs contient une erreur';
+    }
+  }
+}
