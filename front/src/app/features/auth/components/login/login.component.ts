ee66f6fb8d5c9f222a9962aabd7db2f617d55b40
Vincent Danchaud <vdanchaud@gmail.com>
2025-09-30 12:46:39 +0200

    add authService in login component

--- front/src/app/features/auth/components/login/login.component.ts
+++ front/src/app/features/auth/components/login/login.component.ts
@@ -7,6 +7,9 @@ import {
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { LoginRequest } from '../../models/loginRequest.model';
+import { AuthService } from '../../services/auth.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +21,10 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   emailRegex!: RegExp;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -30,6 +36,12 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin(): void {
-    console.log(this.loginForm.value);
+    const loginRequest: LoginRequest = this.loginForm.value as LoginRequest;
+    // console.log(this.loginForm.value);
+
+    this.authService
+      .login(loginRequest)
+      .pipe(tap((value) => console.log(value)))
+      .subscribe();
   }
 }
