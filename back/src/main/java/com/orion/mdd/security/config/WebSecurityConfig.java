e652e0320abed3107534ddc946d17e40d302f7de
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-01 19:30:25 +0200

    correct errors in cors configuration

--- back/src/main/java/com/orion/mdd/security/config/WebSecurityConfig.java
+++ back/src/main/java/com/orion/mdd/security/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
