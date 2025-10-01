f1296e5b6418bc05f0c844a7e420f5db22cee3b1
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-01 18:44:33 +0200

    remove @CrossOrigin annotations and /api prefix

--- back/src/main/java/com/orion/mdd/controller/TopicController.java
+++ back/src/main/java/com/orion/mdd/controller/TopicController.java
@@ -16,16 +16,14 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/topics")
+@RequestMapping("/topics")
 public class TopicController {
 
     private final TopicService topicService;
