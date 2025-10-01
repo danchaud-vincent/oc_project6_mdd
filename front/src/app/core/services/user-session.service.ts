717b0f45301cb3707af7bea1bd62c40898b1b28f
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-01 15:54:37 +0200

    add login, logout, gettoken methods in userSession service

--- front/src/app/core/services/user-session.service.ts
+++ front/src/app/core/services/user-session.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
+import { UserSessionInfo } from '../models/userSessionInfo.model';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserSessionService {
+  private userSessionInfo: UserSessionInfo | undefined;
+  private isLogged = false;
+  private isLoggedSubject = new BehaviorSubject<boolean>(this.isLogged);
 
-  constructor() { }
+  public isLogged$(): Observable<boolean> {
+    return this.isLoggedSubject.asObservable();
+  }
 
+  public login(userInfo: UserSessionInfo): void {
+    this.userSessionInfo = userInfo;
+    this.isLogged = true;
+    this.isLoggedSubject.next(this.isLogged);
+  }
+
+  public logout(): void {
+    this.userSessionInfo = undefined;
+    this.isLogged = false;
+    this.isLoggedSubject.next(this.isLogged);
+  }
+
+  public getToken(): string {
+    return this.userSessionInfo ? this.userSessionInfo.token : '';
+  }
 }
