63abb1916087ef4b844f2b00d5c4f323a714df97
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-01 11:28:44 +0200

    add postsService and posts observable in post-list

--- front/src/app/features/posts/components/post-list/post-list.component.ts
+++ front/src/app/features/posts/components/post-list/post-list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../../models/post.model';
+import { PostsService } from '../../services/posts.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './post-list.component.html',
-  styleUrl: './post-list.component.scss'
+  styleUrl: './post-list.component.scss',
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
+  posts$!: Observable<Post[]>;
 
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.postsService.getPosts();
+  }
 }
