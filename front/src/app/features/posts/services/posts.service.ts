4b855b93069791c3ea4274de8f458f64147fc3cb
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-01 10:09:27 +0200

    add getPostById in posts service

--- front/src/app/features/posts/services/posts.service.ts
+++ front/src/app/features/posts/services/posts.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostsService {
   constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${environment.baseUrl}/posts`);
+  }
+
+  getPostById(postId: number): Observable<Post> {
+    return this.http.get<Post>(`${environment.baseUrl}/posts/${postId}`);
+  }
 }
