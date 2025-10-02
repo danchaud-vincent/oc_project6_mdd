a0387bb096081ded2923cc57c1ef298ac80cd568
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-02 16:21:57 +0200

    use token service in auth interceptor and in userSession service

--- front/src/app/core/interceptors/auth.interceptor.ts
+++ front/src/app/core/interceptors/auth.interceptor.ts
@@ -1,14 +1,14 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Observable } from 'rxjs';
-import { UserSessionService } from '../services/user-session.service';
+import { TokenService } from '../services/token.service';
 
 export function authInterceptor(
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ): Observable<HttpEvent<unknown>> {
-  const userSessionService = inject(UserSessionService);
-  const token = userSessionService.getToken();
+  const tokenService = inject(TokenService);
+  const token = tokenService.getToken();
 
   if (req.url.includes('/auth/login') || req.url.includes('/auth/register')) {
     return next(req);
--- front/src/app/core/services/user-session.service.ts
+++ front/src/app/core/services/user-session.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { UserSessionInfo } from '../models/userSessionInfo.model';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,23 +11,23 @@ export class UserSessionService {
   private isLogged = false;
   private isLoggedSubject = new BehaviorSubject<boolean>(this.isLogged);
 
+  constructor(private tokenService: TokenService) {}
+
   public isLogged$(): Observable<boolean> {
     return this.isLoggedSubject.asObservable();
   }
 
   public login(userInfo: UserSessionInfo): void {
     this.userSessionInfo = userInfo;
+    this.tokenService.setToken(userInfo.token);
     this.isLogged = true;
     this.isLoggedSubject.next(this.isLogged);
   }
 
   public logout(): void {
     this.userSessionInfo = undefined;
+    this.tokenService.removeToken();
     this.isLogged = false;
     this.isLoggedSubject.next(this.isLogged);
-  }
-
-  public getToken(): string {
-    return this.userSessionInfo ? this.userSessionInfo.token : '';
   }
 }
