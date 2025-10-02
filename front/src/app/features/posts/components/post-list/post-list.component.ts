419c23bca339b0826bacb7a1840bd4e15f87349a
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-02 18:50:05 +0200

    add styling, add post-list-item in post-list

--- front/src/app/features/posts/components/post-list/post-list.component.ts
+++ front/src/app/features/posts/components/post-list/post-list.component.ts
@@ -3,13 +3,11 @@ import { Observable } from 'rxjs';
 import { Post } from '../../models/post.model';
 import { PostsService } from '../../services/posts.service';
 import { AsyncPipe } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { c } from '../../../../../../node_modules/@angular/cdk/a11y-module.d-DBHGyKoh';
+import { PostListItemComponent } from '../post-list-item/post-list-item.component';
 
 @Component({
   selector: 'app-post-list',
-  imports: [AsyncPipe, MatCardModule, MatGridListModule],
+  imports: [AsyncPipe, PostListItemComponent],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss',
 })
