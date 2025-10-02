cbaa733220240c4401e7716751ae97da645b7e84
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-02 13:26:05 +0200

    add styling for post-list and mat-card

--- front/src/app/features/posts/components/post-list/post-list.component.ts
+++ front/src/app/features/posts/components/post-list/post-list.component.ts
@@ -3,10 +3,13 @@ import { Observable } from 'rxjs';
 import { Post } from '../../models/post.model';
 import { PostsService } from '../../services/posts.service';
 import { AsyncPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { c } from '../../../../../../node_modules/@angular/cdk/a11y-module.d-DBHGyKoh';
 
 @Component({
   selector: 'app-post-list',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MatCardModule, MatGridListModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss',
 })
