aad9f1a2eafc2d38adcaf3f9abd873b6cbbd222d
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-02 15:40:33 +0200

    clean auth interceptor

--- front/src/app/core/interceptors/auth.interceptor.ts
+++ front/src/app/core/interceptors/auth.interceptor.ts
@@ -1,9 +1,4 @@
-import {
-  HttpEvent,
-  HttpHandlerFn,
-  HttpHeaders,
-  HttpRequest,
-} from '@angular/common/http';
+import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserSessionService } from '../services/user-session.service';
