598ae3e8ce663157e2df1797a5c277fdbc2382d1
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-02 18:50:51 +0200

    add Input to get data from post-list

--- front/src/app/features/posts/components/post-list-item/post-list-item.component.ts
+++ front/src/app/features/posts/components/post-list-item/post-list-item.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Post } from '../../models/post.model';
 
 @Component({
   selector: 'app-post-list-item',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './post-list-item.component.html',
-  styleUrl: './post-list-item.component.scss'
+  styleUrl: './post-list-item.component.scss',
 })
 export class PostListItemComponent {
-
+  @Input() post!: Post;
 }
