d6928b2ca4331081be572a8637b29c91c5f24742
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-02 19:18:27 +0200

    add new styling for post-list and post-list item, correct html element in post-list html

--- front/src/app/features/posts/components/post-list-item/post-list-item.component.ts
+++ front/src/app/features/posts/components/post-list-item/post-list-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Post } from '../../models/post.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-post-list-item',
-  imports: [MatCardModule],
+  imports: [MatCardModule, DatePipe],
   templateUrl: './post-list-item.component.html',
   styleUrl: './post-list-item.component.scss',
 })
