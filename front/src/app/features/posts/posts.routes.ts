08c9f304a9a00e7d77ab82ff7e45b6c7a789b139
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-03 11:00:07 +0200

    add single post in posts.routes to display a post by id

--- front/src/app/features/posts/posts.routes.ts
+++ front/src/app/features/posts/posts.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { LayoutWithHeadersComponent } from '../../core/components/layouts/layout-with-headers/layout-with-headers.component';
+import { SinglePostComponent } from './components/single-post/single-post.component';
 
 export const POSTS_ROUTES: Routes = [
   {
@@ -10,6 +11,10 @@ export const POSTS_ROUTES: Routes = [
       {
         path: '',
         component: PostListComponent,
+      },
+      {
+        path: ':id',
+        component: SinglePostComponent,
       },
     ],
   },
