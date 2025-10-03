cc75eceaf6b97ea0e9aa9ff6692c30b12f3ed595
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-03 18:15:58 +0200

    create topic-list-item component

---
+++ front/src/app/features/topics/components/topic-list-item/topic-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-list-item',
+  imports: [],
+  templateUrl: './topic-list-item.component.html',
+  styleUrl: './topic-list-item.component.scss'
+})
+export class TopicListItemComponent {
+
+}
