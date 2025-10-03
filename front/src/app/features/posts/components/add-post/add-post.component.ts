9a4ff528652cbe5ed693f7acdbb72b2293606390
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-03 17:28:20 +0200

    add button component in add post componennt

--- front/src/app/features/posts/components/add-post/add-post.component.ts
+++ front/src/app/features/posts/components/add-post/add-post.component.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-add-post',
@@ -13,6 +14,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    ButtonComponent,
   ],
   templateUrl: './add-post.component.html',
   styleUrl: './add-post.component.scss',
