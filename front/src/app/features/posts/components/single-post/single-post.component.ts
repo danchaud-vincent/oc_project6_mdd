2eb55ef4b5dd2617e0b7ebd7be42f4a40a3b72e4
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-03 11:54:13 +0200

    add postsService with GetPostById to get the post data, add styling, add html content with async pipe and date pipe

--- front/src/app/features/posts/components/single-post/single-post.component.ts
+++ front/src/app/features/posts/components/single-post/single-post.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ButtonBackwardComponent } from '../../../../shared/components/button-backward/button-backward.component';
+import { Post } from '../../models/post.model';
+import { Observable } from 'rxjs';
+import { PostsService } from '../../services/posts.service';
+import { ActivatedRoute } from '@angular/router';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-single-post',
-  imports: [],
+  imports: [ButtonBackwardComponent, AsyncPipe, DatePipe],
   templateUrl: './single-post.component.html',
-  styleUrl: './single-post.component.scss'
+  styleUrl: './single-post.component.scss',
 })
-export class SinglePostComponent {
+export class SinglePostComponent implements OnInit {
+  post$!: Observable<Post>;
 
+  constructor(
+    private postsService: PostsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const postId = this.route.snapshot.params['id'];
+    this.post$ = this.postsService.getPostById(postId);
+  }
 }
