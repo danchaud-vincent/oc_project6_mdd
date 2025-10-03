540299c0495e9bf81d78376310a16379802b039a
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-03 16:30:20 +0200

    add AddPostComponent in posts routes

--- front/src/app/features/posts/posts.routes.ts
+++ front/src/app/features/posts/posts.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { LayoutWithHeadersComponent } from '../../core/components/layouts/layout-with-headers/layout-with-headers.component';
 import { SinglePostComponent } from './components/single-post/single-post.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
 
 export const POSTS_ROUTES: Routes = [
   {
@@ -11,6 +12,10 @@ export const POSTS_ROUTES: Routes = [
       {
         path: '',
         component: PostListComponent,
+      },
+      {
+        path: 'create',
+        component: AddPostComponent,
       },
       {
         path: ':id',
