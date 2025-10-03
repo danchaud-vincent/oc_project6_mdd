7b51df2194aad2d9bc0eb9bbddba1b4ec9303909
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-03 18:32:02 +0200

    create getTopics method in topics service

--- front/src/app/features/topics/services/topics.service.ts
+++ front/src/app/features/topics/services/topics.service.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Topic } from '../models/topic.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TopicsService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getTopics(): Observable<Topic[]> {
+    return this.http.get<Topic[]>(`${environment.baseUrl}/topics`);
+  }
 }
