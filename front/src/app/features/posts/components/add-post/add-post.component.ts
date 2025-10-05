c260083163777b3285080bd3318425afc1300fd9
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-05 16:49:43 +0200

    add validators in AddPost form

--- front/src/app/features/posts/components/add-post/add-post.component.ts
+++ front/src/app/features/posts/components/add-post/add-post.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ButtonBackwardComponent } from '../../../../shared/components/button-backward/button-backward.component';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { Topic } from '../../../topics/models/topic.model';
+import { TopicsService } from '../../../topics/services/topics.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-post',
@@ -15,21 +24,28 @@ import { ButtonComponent } from '../../../../shared/components/button/button.com
     MatInputModule,
     MatSelectModule,
     ButtonComponent,
+    AsyncPipe,
   ],
   templateUrl: './add-post.component.html',
   styleUrl: './add-post.component.scss',
 })
 export class AddPostComponent implements OnInit {
   addPostForm!: FormGroup;
+  topics$!: Observable<Topic[]>;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private topicsService: TopicsService
+  ) {}
 
   ngOnInit(): void {
     this.addPostForm = this.formBuilder.group({
-      topic: [null],
-      title: [null],
-      content: [null],
+      topic: [null, [Validators.required]],
+      title: [null, [Validators.required]],
+      content: [null, Validators.required],
     });
+
+    this.topics$ = this.topicsService.getTopics();
   }
 
   onSubmitForm() {
