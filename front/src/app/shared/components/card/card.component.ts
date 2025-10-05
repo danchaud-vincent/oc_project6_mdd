4fff4a7179aac22d20d38d2a434fb2ae55d33cd0
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-05 18:10:21 +0200

    add ng-content and mat-card in card component

--- front/src/app/shared/components/card/card.component.ts
+++ front/src/app/shared/components/card/card.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
-export class CardComponent {
-
-}
+export class CardComponent {}
