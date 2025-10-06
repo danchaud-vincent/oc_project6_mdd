ff81be3cb584003be99ad4b4e6bf0938f70b8213
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-06 19:38:43 +0200

    update topic-list component by adding button and scss

--- front/src/app/features/topics/components/topic-list-item/topic-list-item.component.ts
+++ front/src/app/features/topics/components/topic-list-item/topic-list-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Topic } from '../../models/topic.model';
 import { CardComponent } from '../../../../shared/components/card/card.component';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-topic-list-item',
-  imports: [CardComponent],
+  imports: [CardComponent, ButtonComponent],
   templateUrl: './topic-list-item.component.html',
   styleUrl: './topic-list-item.component.scss',
 })
