f22c2a549c8dfd9382b1264e8dcd183bac187e0f
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-06 12:21:07 +0200

    add form with formbuilder in Me Component html, css, and ts

--- front/src/app/features/me/components/me/me.component.ts
+++ front/src/app/features/me/components/me/me.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { passwordValidator } from '../../../auth/validators/password.validator';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-me',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ButtonComponent,
+  ],
   templateUrl: './me.component.html',
-  styleUrl: './me.component.scss'
+  styleUrl: './me.component.scss',
 })
-export class MeComponent {
+export class MeComponent implements OnInit {
+  meForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.meForm = this.formBuilder.group({
+      username: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required, passwordValidator()]],
+    });
+  }
 }
