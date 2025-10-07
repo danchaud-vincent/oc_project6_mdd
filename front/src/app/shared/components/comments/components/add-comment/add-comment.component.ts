7663eaa332160e68b7cb356182c9a4b97a3064af
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-07 18:42:36 +0200

    add output and submitform

--- front/src/app/shared/components/comments/components/add-comment/add-comment.component.ts
+++ front/src/app/shared/components/comments/components/add-comment/add-comment.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIcon } from '@angular/material/icon';
@@ -10,4 +15,19 @@ import { MatIcon } from '@angular/material/icon';
   templateUrl: './add-comment.component.html',
   styleUrl: './add-comment.component.scss',
 })
-export class AddCommentComponent {}
+export class AddCommentComponent implements OnInit {
+  addComment!: FormGroup;
+  @Output() newComment = new EventEmitter<string>();
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.addComment = this.formBuilder.group({
+      comment: [null, [Validators.required]],
+    });
+  }
+
+  onSubmitForm(): void {
+    console.log(this.addComment.value.comment);
+  }
+}
