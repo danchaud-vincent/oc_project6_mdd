e6ad0af757a12a8d1d9fe698031315c4118e7460
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-07 11:17:46 +0200

    use forkJoin to get topics with subscriptions status

--- front/src/app/features/topics/components/topic-list/topic-list.component.ts
+++ front/src/app/features/topics/components/topic-list/topic-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, map, Observable, switchMap } from 'rxjs';
 import { Topic } from '../../models/topic.model';
 import { TopicsService } from '../../services/topics.service';
 import { AsyncPipe } from '@angular/common';
@@ -13,10 +13,26 @@ import { TopicListItemComponent } from '../topic-list-item/topic-list-item.compo
 })
 export class TopicListComponent implements OnInit {
   topics$!: Observable<Topic[]>;
+  topicsSubscribed$!: Observable<Topic[]>;
 
   constructor(private topicsService: TopicsService) {}
 
   ngOnInit(): void {
-    this.topics$ = this.topicsService.getTopics();
+    this.topics$ = forkJoin({
+      topics: this.topicsService.getTopics(),
+      subscriptions: (this.topicsSubscribed$ =
+        this.topicsService.getTopicsSubscriptions()),
+    }).pipe(
+      map(({ topics, subscriptions }) => {
+        return topics.map((topic) => {
+          return {
+            ...topic,
+            isSubscribed: subscriptions.some(
+              (subscription) => subscription.id === topic.id
+            ),
+          };
+        });
+      })
+    );
   }
 }
