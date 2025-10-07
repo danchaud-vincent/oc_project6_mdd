b3357aa1760a2473e2fe2485ef1bcb147caf18af
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-07 16:33:38 +0200

    add shorten pipe in posts list component

--- front/src/app/features/posts/components/post-list-item/post-list-item.component.ts
+++ front/src/app/features/posts/components/post-list-item/post-list-item.component.ts
@@ -4,10 +4,11 @@ import { Post } from '../../models/post.model';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { CardComponent } from '../../../../shared/components/card/card.component';
+import { ShortenPipe } from '../../../../shared/pipes/shorten.pipe';
 
 @Component({
   selector: 'app-post-list-item',
-  imports: [MatCardModule, DatePipe, CardComponent],
+  imports: [MatCardModule, DatePipe, CardComponent, ShortenPipe],
   templateUrl: './post-list-item.component.html',
   styleUrl: './post-list-item.component.scss',
 })
