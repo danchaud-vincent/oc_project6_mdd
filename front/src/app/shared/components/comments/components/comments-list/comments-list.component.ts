80cbc332c355f013d4e400e9704ba69a71519c67
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-07 17:59:13 +0200

    create the html for the comments, add styling and imports

--- front/src/app/shared/components/comments/components/comments-list/comments-list.component.ts
+++ front/src/app/shared/components/comments/components/comments-list/comments-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { Comment } from '../../models/comment.model';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-comments-list',
-  imports: [],
+  imports: [MatListModule, MatGridListModule, DatePipe],
   templateUrl: './comments-list.component.html',
   styleUrl: './comments-list.component.scss',
 })
 export class CommentsListComponent {
-  @Input() comments!: string;
+  @Input() comments!: Comment[];
 }
