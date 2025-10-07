f66ff17ea60123fbd274077f283a1c358a8ac23d
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-07 10:23:42 +0200

    add Input isUserProfile to know if the component is used in user page or not, to deal with user subscriptions

--- front/src/app/features/topics/components/topic-list-item/topic-list-item.component.ts
+++ front/src/app/features/topics/components/topic-list-item/topic-list-item.component.ts
@@ -11,4 +11,25 @@ import { ButtonComponent } from '../../../../shared/components/button/button.com
 })
 export class TopicListItemComponent {
   @Input() topic!: Topic;
+  @Input() isUserProfile: boolean = false;
+
+  getSubscriptionLabel(): string {
+    if (this.isUserProfile && this.topic.isSubscribed) {
+      return 'Se désabonner';
+    }
+
+    return this.topic.isSubscribed ? 'Déjà abonné' : "S'abonner";
+  }
+
+  isDisabled(): boolean {
+    if (this.isUserProfile) {
+      return false;
+    }
+
+    if (this.topic.isSubscribed) {
+      return true;
+    }
+
+    return false;
+  }
 }
