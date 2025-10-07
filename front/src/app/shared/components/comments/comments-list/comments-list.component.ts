ec19505529017e3102d8268dc0db518cbe40db81
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-07 16:44:43 +0200

    add input var in comments-list

--- front/src/app/shared/components/comments/comments-list/comments-list.component.ts
+++ front/src/app/shared/components/comments/comments-list/comments-list.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-comments-list',
   imports: [],
   templateUrl: './comments-list.component.html',
-  styleUrl: './comments-list.component.scss'
+  styleUrl: './comments-list.component.scss',
 })
 export class CommentsListComponent {
-
+  @Input() comments!: string;
 }
