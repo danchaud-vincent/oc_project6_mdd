52c614655636050691758d848b67fc9da6d8ff2f
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-08 10:41:17 +0200

    add onPostCommented method which get the event emitted by the new comment and add it into the base, refresh the comments in post

--- front/src/app/features/posts/components/single-post/single-post.component.ts
+++ front/src/app/features/posts/components/single-post/single-post.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonBackwardComponent } from '../../../../shared/components/button-backward/button-backward.component';
 import { Post } from '../../models/post.model';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, switchMap } from 'rxjs';
 import { PostsService } from '../../services/posts.service';
 import { ActivatedRoute } from '@angular/router';
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { CommentsListComponent } from '../../../../shared/components/comments/components/comments-list/comments-list.component';
 import { CommentsService } from '../../services/comments.service';
 import { Comment } from '../../../../shared/components/comments/models/comment.model';
+import { CommentRequest } from '../../models/commentRequest.model';
 
 @Component({
   selector: 'app-single-post',
@@ -34,5 +35,20 @@ export class SinglePostComponent implements OnInit {
     const postId = this.route.snapshot.params['id'];
     this.post$ = this.postsService.getPostById(postId);
     this.comments$ = this.commentsService.getCommentsByPost(postId);
+  }
+
+  onPostCommented(newCommentValue: string) {
+    const postId = this.route.snapshot.params['id'];
+    const commentRequest: CommentRequest = { content: newCommentValue };
+
+    this.comments$ = this.commentsService
+      .addCommentToPost(postId, commentRequest)
+      .pipe(
+        switchMap(() => this.commentsService.getCommentsByPost(postId)),
+        catchError((err) => {
+          console.error("Erreur lors de l'ajout du commentaire: ", err);
+          return of([]);
+        })
+      );
   }
 }
