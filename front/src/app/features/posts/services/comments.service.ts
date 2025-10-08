22a563c9555ae85f7fb2a3294feac780a67e11f6
Vincent Danchaud <vdanchaud@gmail.com>
2025-10-08 10:40:28 +0200

    create addCommentToPost method in commentsservice

--- front/src/app/features/posts/services/comments.service.ts
+++ front/src/app/features/posts/services/comments.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Comment } from '../../../shared/components/comments/models/comment.model';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { CommentRequest } from '../models/commentRequest.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,16 @@ export class CommentsService {
   getCommentsByPost(postId: number): Observable<Comment[]> {
     return this.http.get<Comment[]>(
       `${environment.baseUrl}/posts/${postId}/comments`
+    );
+  }
+
+  addCommentToPost(
+    postId: number,
+    commentRequest: CommentRequest
+  ): Observable<Comment> {
+    return this.http.post<Comment>(
+      `${environment.baseUrl}/posts/${postId}/comments`,
+      commentRequest
     );
   }
 }
